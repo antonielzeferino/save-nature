5911f45f3e2badfda61de319a29697f8584038df
Antoniel <antonielzeferino0@gmail.com>
2024-10-21 09:05:05 -0300

    [style] cards styel fix

--- src/components/EcoFriendlyActionsList.tsx
+++ src/components/EcoFriendlyActionsList.tsx
@@ -13,7 +13,7 @@ const EcoFriendlyActions = [
 
 const EcoFriendlyActionsList: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8">
       {EcoFriendlyActions.map(action => (
         <div 
           key={action.id}
@@ -27,7 +27,7 @@ const EcoFriendlyActionsList: React.FC = () => {
             objectFit="cover" 
             className="absolute inset-0 transition duration-300 ease-in-out" 
           />
-          <h5 className="relative z-10 text-stone-300 text-2xl md:text-3xl p-2 text-center rounded-lg bg-black/90">
+          <h5 className="relative z-10 text-stone-300 text-2xl md:text-3xl p-2 text-center rounded-lg  bg-black/20 w-[85%]">
             {action.text}
           </h5>
         </div>
