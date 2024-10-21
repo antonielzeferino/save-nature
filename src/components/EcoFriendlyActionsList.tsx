9249a94818f8eece1cef1b7cd8a24de87d0f945e
Antoniel Zeferino <114781545+antonielzeferino@users.noreply.github.com>
2024-10-21 08:52:41 -0300

    Update EcoFriendlyActionsList.tsx
    [style] fix card responsibility

--- src/components/EcoFriendlyActionsList.tsx
+++ src/components/EcoFriendlyActionsList.tsx
@@ -13,7 +13,7 @@ const EcoFriendlyActions = [
 
 const EcoFriendlyActionsList: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {EcoFriendlyActions.map(action => (
         <div 
           key={action.id}
